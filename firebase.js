cf0a2575fbef0baee1c4326e3a35bccdc157ef5e
grisskos <christian.bueno1706@gmail.com>
2025-11-27 16:16:18 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -10,14 +10,14 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 // SUBSTITUA "SUA_API_KEY" PELOS SEUS DADOS REAIS DO FIREBASE CONSOLE
-const firebaseConfig = {
-	apiKey: "AIzaSyAANME6i8psLUlX13AAi3PBgnd0IIvaY5Y",
-	authDomain: "ficharpg-edc5c.firebaseapp.com",
-	projectId: "ficharpg-edc5c",
-	storageBucket: "ficharpg-edc5c.firebasestorage.app",
-	messagingSenderId: "522630132698",
-	appId: "1:522630132698:web:30d3dc6f103916d70246e3",
-	measurementId: "G-6PJKQYBRRC
+const firebaseConfig = {	
+  apiKey: "AIzaSyAANME6i8psLUlX13AAi3PBgnd0IIvaY5Y",
+  authDomain: "ficharpg-edc5c.firebaseapp.com",
+  projectId: "ficharpg-edc5c",
+  storageBucket: "ficharpg-edc5c.firebasestorage.app",
+  messagingSenderId: "522630132698",
+  appId: "1:522630132698:web:30d3dc6f103916d70246e3",
+  measurementId: "G-6PJKQYBRRC"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -29,3 +29,4 @@ export {
   query, where, onSnapshot, orderBy, onAuthStateChanged,
   signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut
 };
+
